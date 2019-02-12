413ba481111f7a13ce98c942dc3a99436e0558e3
h571544 <fredrik.1997@hotmail.com>
2019-02-12 13:56:09 +0100

    Spiller ferdig?

--- src/no/hvl/dat109/Spiller.java
+++ src/no/hvl/dat109/Spiller.java
@@ -4,7 +4,28 @@ public class Spiller {
     private String navn;
     private Integer verdi;
 
-    public void spill(Kopp kopp){
+    public Spiller(String navn, Integer verdi) {
+        this.navn = navn;
+        this.verdi = verdi;
+    }
 
+    public void spill(Kopp kopp){
+        verdi = kopp.trill();
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public Integer getVerdi() {
+        return verdi;
+    }
+
+    public void setVerdi(Integer verdi) {
+        this.verdi = verdi;
     }
 }
